fe78f512868783eaccc0609c440514c37788ea14
Camet <alan_israel_97@hotmail.com>
2023-03-27 23:24:17 -0600

    intenté agregar evento a js sin exito

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 //Variables
+const screenInputAcount = document.getElementsByClassName('screenInputAcount')
+const screenInputNip = document.getElementsByClassName('screenInputNip')
 const uno = document.getElementById('uno')
 const dos = document.getElementById('dos')
 const tres = document.getElementById('tres')
@@ -13,3 +15,14 @@ const tripleCero = document.getElementById('tripleCero')
 const enter = document.getElementById('enter')
 const clear = document.getElementById('clear')
 const cancel = document.getElementById('cancel')
+
+//Eventos
+uno.onclick = function unoEvent() {
+    screenInputAcount.textContent = screenInputAcount.textContent + "1"
+
+}
+
+dos.onclick = function (e) {
+    screenInputAcount.textContent = screenInputAcount.textContent + "2"
+
+}
