c289ed077393f8357791d114b5dc2f0efcff0dd6
Camet <alan_israel_97@hotmail.com>
2023-05-21 21:23:16 -0600

    feat: Se creo la funcion de limpiar el screen

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-//Variables
 const cardsContainer = document.querySelector("#cardsContainer")
-
 let data =[];
 
+//consumo de JSON
 async function getJSON(customer){
     let request = await fetch('customers.json')
     let response = await request.json()
@@ -12,18 +11,18 @@ async function getJSON(customer){
 }
 getJSON()
 
+//creando los usuarios dentro del array data
 function createData(usuario, i){
     const newUser = {
         name: usuario.name,
         balance: usuario.balance,
         acount: usuario.acount,
         nip: usuario.nip
-        }
+    }
     data.push(newUser)
     printCard(i)
     
 }
-
 
 function printCard(i){
     let img = document.createElement("img")
@@ -52,77 +51,14 @@ function printCard(i){
     div.appendChild(banckName)
     div.appendChild(divText)
     cardsContainer.appendChild(div)
-
-    {/* <div class="card">
-            <img class="profileImg" src="assets/profile.png" alt="profile">
-            <p class="bankName">DEV.f Bank</p>
-            <div class="data">
-                <div class="textALign">
-                    <p class="nameCard">Name: </p>
-                    <p class="name nameDos"></p>
-                </div>
-                <div class="textALign">
-                    <p class="acountCard">Acount: </p>
-                    <p class="acount acountDos"></p>
-                </div>
-                <div class="textALign">
-                    <p class="nipCard">NIP: </p>
-                    <p class="nip nipDos"></p>
-                </div>
-            </div>
-        </div> */}
 }
-
-/* const display = [
-    {name: nameUno,
-    acount: acountUno,
-    nip: nipUno
-    },
-
-    {name: nameDos,
-    acount: acountDos,
-    nip: nipDos
-    },
-
-    {name: nameTres,
-    acount: acountTres,
-    nip: nipTres
-    }
-] */
-const boton = document.querySelector(".botonLogin")
-const buttonExit = document.querySelector(".buttonExit")
-const screenInputAcount = document.querySelector(".screenInputAcount")
-const uno =document.querySelector(".uno")
+ 
+const screen = document.querySelector(".contentScreen")
+const enter = document.querySelector("#enter")
 
 
-//Eventos
-function exitEvent(){
-    window.location = "index.html"
+function cleanScreen(){
+    screen.innerHTML = ''
+    console.log("hola")
 }
-buttonExit.onclick = exitEvent
-
-function textUno(string,display){
-    display.name.textContent = string.name;
-    display.acount.textContent = string.acount;
-    display.nip.textContent = string.nip;
-} 
-/* textUno(customers[0], display[0])
-textUno(customers[1], display[1])
-textUno(customers[2], display[2]) */
-
-function login(){
-    
-    let userAcount = document.querySelector(".screenInputAcount").value
-    let userNip = document.querySelector(".screenInputNip").value
-    
-    if(userAcount == customers[0].acount && userNip == customers[0].nip){
-        window.location.href = "customerOne.html"
-    } else if(userAcount == customers[1].acount && userNip == customers[1].nip){
-        window.location.href = "customerDos.html"
-    } else if(userAcount == customers[2].acount && userNip == customers[2].nip){
-        window.location.href = "customerTres.html"
-    }else {
-        alert("Acount or NIP is not valid.")
-    } 
-}
-boton.onclick = login 
+enter.addEventListener("click", cleanScreen)
